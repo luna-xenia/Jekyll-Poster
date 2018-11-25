a7cb5ad096547a44a6286882230aa01c90821748
slipdip123 <38812289+slipdip123@users.noreply.github.com>
2018-11-25 20:15:15 +0000

    Added GUI

--- main.py
+++ main.py
@@ -1,30 +1,64 @@
-def addContent(z):
-    print("Adding content...")
-    file.write(z)
+from tkinter import *
+
+root = Tk()
 
 
-def makeFile(y, x):
+def makePost(x, y, z):
     global file
+
     print("Making file...")
     file = open(x + ".md", "a")
     print("Made file.")
+
     print("Adding front...")
     file.write("---\nlayout: post\ntitle: " + y + "\n---\n\n")
     print("Added front.")
 
+    print("Adding content...")
+    file.write(z)
 
-def start():
+
+def retrieve_input():
     global x
     global y
     global z
-    y = input("What should the title be? ")
-    xTemp = input("What is the date? (YYYY-MM-DD)")
+
+    # date
+    xTemp = textBox.get("1.0", "end-1c")
+    # title
+    y = textBox2.get("1.0", "end-1c")
+    # content
+    z = textBox3.get("1.0", "end-1c")
+    # file name
     x = xTemp + "-" + y
-    makeFile(y, x)
-    z = input("Write the post contents with a [backslash]n for a newline.")
-    addContent(z)
+
+    makePost(x, y, z)
 
 
-print("Welcome to the Poster. It is assumed this python file is in _posts.")
-input("Press <--/ to start")
-start()
+T = Text(root, height=2, width=30)
+T.pack()
+T.insert(END, "Enter the date\nYYYY-MM-DD\n")
+
+textBox = Text(root, height=1, width=40)
+textBox.pack()
+
+X = Text(root, height=1, width=30)
+X.pack()
+X.insert(END, "Enter the title")
+
+textBox2 = Text(root, height=1, width=40)
+textBox2.pack()
+
+Y = Text(root, height=1, width=30)
+Y.pack()
+Y.insert(END, "Enter the content")
+
+textBox3 = Text(root, height=40, width=120)
+textBox3.pack()
+
+buttonMake = Button(root, height=1, width=10, text="Finish",
+                    command=lambda: retrieve_input())
+#command=lambda: retrieve_input() >>> just means do this when i press the button
+buttonMake.pack()
+
+mainloop()
